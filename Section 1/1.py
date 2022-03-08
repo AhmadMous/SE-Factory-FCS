79ee7c4e1a3f835b5fba6abb1953aeb2f703d2c5
AhmadMous <badmass95@gmail.com>
2022-03-08 17:38:56 +0200

    submit excercise 1

---
+++ Section 1/1.py
@@ -0,0 +1,8 @@
+# Implement a function that takes two integers as a
+# parameter and returns a string with the sum of the two numbers.
+# Example: for a=2 and b=3
+# Expected output: 2+3=5
+
+def calculate_sum(a,b):
+    sum = a + b
+    return str(a) + "+" + str(b) + "=" + str(sum)
