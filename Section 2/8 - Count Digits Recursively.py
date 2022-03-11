2775982d99dc28a66c7bbe688d623cd67c8af066
AhmadMous <badmass95@gmail.com>
2022-03-12 00:58:13 +0200

    Add exc.8 guidance

--- Section 2/8 - Count Digits Recursively.py
+++ Section 2/8 - Count Digits Recursively.py
@@ -1,7 +1,11 @@
+# Thought process:
+# - Number of digits of n is number of digits of 1+ n // 10
+# - Base case is when n == 0, that's where number of digits returns 0
+# - Recursive loop ends when n == 0
+
+
 def count_digits(n):
     if n == 0:
         return 0
-    elif (n//10) == 0:
-        return 1
     else:
-        return 1 + count_digits(n//10)
+        return 1 + count_digits(n // 10)
