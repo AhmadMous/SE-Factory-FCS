34c45b54df5c27f2e06c528577e22a3cc0bb4336
AhmadMous <badmass95@gmail.com>
2022-03-12 00:51:36 +0200

    Add exc.6 guidance

--- Section 2/6 - Perfect Number.py
+++ Section 2/6 - Perfect Number.py
@@ -1,23 +1,15 @@
+# Thought process:
+# - If b divides a, then a % b == 0
+# - All divisors of a are equal to or less than k = (a // 2) + 1
+# - Loop over numbers less than a adding their value to a sum
+# - If sum == number then it is perfect, return True, else return False
+
 def perfect(number):
-    #The variable "value" represent the value of the integer number entered by the user
-    #Decalre a bool variable isperfect and initialize it to false
-    is_perfect = False
    # Declare a variable sum and initialize it to zero
     sum = 0
-    #1- Check if "number" is non-positive.
-    if number < 0 :
-        positive = False
-    else:
-        positive = True
-    #2- If not, find the proper divisors of "number" and find their sum
-    outstr = ''
-    sum = 0
-    if positive == True:
-        for i in range(1, ((number//2)+1)):
-            if number%i == 0:
-                outstr += ' + ' + str(i)
-                sum += i
-   #-3- Check if their sum is equal to "number"  
-    if sum == number:
-        is_perfect = True
-    return is_perfect
+
+    for divisor in range(1, ((number // 2) + 1)):
+        if number % divisor == 0:
+            sum += divisor
+ 
+    return sum == number
