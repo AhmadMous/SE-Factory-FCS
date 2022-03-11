dea5e6b82686400994819d0b06d0cee68d988a2e
AhmadMous <badmass95@gmail.com>
2022-03-12 00:55:21 +0200

    Add exc.7 guidance

--- Section 2/7 - Recursive Sum.py
+++ Section 2/7 - Recursive Sum.py
@@ -1,5 +1,10 @@
+# Thought process:
+# - Sum from 1 to n is equal to n + sum from 1 to n - 1
+# - A base case is when n == 0, where it automatically returns n
+# - The recursive loop ends when n is equal to 0
+
 def recursive_sum(n):
     if n == 0:
         return 0
     else:
-        return n + recursive_sum(n-1)
+        return n + recursive_sum(n - 1)
