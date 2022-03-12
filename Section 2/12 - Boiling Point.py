3b2af633e4abe0593a4dbfeb297ebed1f4307e44
AhmadMous <badmass95@gmail.com>
2022-03-12 18:49:51 +0200

    Add exc 12 guidance

--- Section 2/12 - Boiling Point.py
+++ Section 2/12 - Boiling Point.py
@@ -1,19 +1,31 @@
+# Thought process:
+# - We must iterate over elements finding their boiling point and acceptable range
+# - Element's bioling point with the smallest difference is the match
+# - If no elements match then the closest match will be "Unknown" by default
+# - Return the closest match
+
 def boiling_material(boiling_map, boiling_point):
-    min_distance = 999999
-    found = False
+
+    # Default for closest element and difference between it's boiling point and the given one
+    smallest_difference = 999999
+    closest = "Unknown"
     
-    for element in boiling_map.keys():
-        elem_bp = boiling_map[element]
-        max_eps= round(abs(elem_bp)* 0.05, 9)
-        eps = round(abs(elem_bp - boiling_point), 9)
+    # Iterate over elements in the boiling map
+    for element in boiling_map:
+
+        # Max difference between a matching element's boiling point and given one is 5% of element's bp
+        element_bp = boiling_map[element]
+        max_difference = round(abs(element_bp) * 0.05, 9)
+
+        # Find the difference between given and element's boiling point
+        difference = round(abs(element_bp - boiling_point), 9)
+
+        # If it is within acceptable range, find if it has smallest difference
+        if difference <= max_difference:
+            if difference <= smallest_difference:
+
+                # If it has smallest difference, then it is the closest
+                smallest_difference = difference
+                closest = element
         
-        if eps <= max_eps:
-            found = True
-            if eps <= min_distance:
-                min_distance = eps
-                closest_mat = element
-        
-    if found:
-        return closest_mat
-    else:
-        return "Unknown"
+    return closest
