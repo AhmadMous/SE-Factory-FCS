354ef05cab4c2fca1d9fd2456e84f86daa41b3d2
AhmadMous <badmass95@gmail.com>
2022-03-12 17:20:11 +0200

    Add exc 11 guidance

--- Section 2/10 - Palindrome.py
+++ Section 2/10 - Palindrome.py
@@ -1,22 +1,32 @@
+# Thought process:
+# - A palindrome string is equal to its reverse, thus all characters from
+# the right of the midpoint charachter have a mirror element in the left
+# - If the whole string is a palindrome then the whole string is the maximum sub palindrome
+# else, try to find palindrome whose length is 1 character smaller.
+# Try this repeatedly till you find one or it becomes 1 character, which is a palindrome by default
+# e.g. stabba, try for stabba, then stabb, tabba, then stab, tabb, abba => palindrome found!
+
 def palindrome(string):
+    
+    # Iterate up to the midpoint of the string
     n = len(string)
     for char in range(n // 2):
+
+        # If any character does not have an equal opposite index charachter, return False
         if string[char] != string[-1 - char]:
             return False
+
+    # If every character has a mirror image, then return True
     return True
 
 def max_sub_palindrome(string):
-    strlen = len(string)
-    if palindrome(string):
-        return string
 
-    else:
-        for i in range(0, strlen):
-            x = string[:strlen-1-i]
-            if len(x) == 1:
-                return x
-            else:
-                for j in range(1+i):
-                    y = string[j:strlen-i+j]
-                    if palindrome(y):
-                        return y
+    # Iterate over different lengths of substrings
+    strlen = len(string)
+    for i in range(strlen):
+
+        # Iterate over all substrings of length strlen - i, meaning it starts with whole string for i == 0
+        for j in range(1 + i):
+            substring = string[j : strlen - i + j]
+            if palindrome(substring):
+                return substring
--- Section 2/11 - Coprimes.py
+++ Section 2/11 - Coprimes.py
@@ -1,17 +1,27 @@
-    doing this slowly will result in : your code took too long to execute
+# Thought process:
+# - We can find if two numbers are coprimes by iterating up to the smaller of the two,
+# till we make sure there are no common divisors of both but 1, however doing this will result
+# in a message saying the code took too long to compute when attempting to count coprimes
+# - An alternate way of find if 2 numbers are coprimes is by attempting to find the GCD, if it is
+# equal to 1, then they are coprimes. A fast way of finding GCD is by using Euclid's algorithm which
+# is taught in discrete mathematics. It is also mentioned in "Grokking Algorithms" book
+# - You can read about Euclid's algorithm here : https://www.khanacademy.org/computing/computer-science/cryptography/modarithmetic/a/the-euclidean-algorithm
 
 
 def coprime(a, b):
 
+    # Find which of the two is higher and which is smaller
     lowest = a if a < b else b
     highest = a if a >= b else b
 
+    # Euclid's algorithm for GCD
     def GCD(larger, smaller):
         if smaller == 0:
             return larger
         else:
             return GCD(smaller, larger % smaller)
         
+    # IF GCD == 1, coprimes
     if GCD(highest, lowest) == 1:
         return True
     else:
@@ -19,8 +29,13 @@ def coprime(a, b):
     
             
 def count_coprimes(n):
+
+    # Variable to hold numbers of coprimes found, which will be returned by the function
     coprimes = 0
-    for number in range(1, n+1):
+
+    # Iterate over numbers up to n + 1 to find all coprimes for k starting from 1 and up to n inclusive
+    for number in range(1, n + 1):
         if coprime(n, number):
             coprimes += 1
+            
     return coprimes
